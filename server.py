d541763db9ba88e69e4c83607c98ff7283a2af9d
Your Name <you@example.com>
2025-12-29 17:27:21 -0500

    all

--- server.py
+++ server.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("EmotionDetection")
+app = Flask(__name__)
+
+import os
+
+print("TEMPLATE FOLDER:", app.template_folder)
+print("FILES:", os.listdir(app.template_folder))
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
@@ -11,6 +16,8 @@ def emotion_detector_route():
 
     # Pass the text to the sentiment_analyzer function and store the response
     emotion_data = emotion_detector(text_to_analyze)
+    if emotion_data['dominant_emotion'] is None:
+        return "<b>Invalid text! Please try again!</b>"
     emotion_data_list = []
     for key, value in emotion_data.items():
         if key == 'dominant_emotion':
@@ -18,7 +25,7 @@ def emotion_detector_route():
         emotion_data_list.append(f"'{key}': {value}")
     
     emotion_data_str = ", ".join(emotion_data_list)
-    emotion_data_str += f". The dominant emotion is {emotion_data['dominant_emotion']}"
+    emotion_data_str += f". The dominant emotion is <b>{emotion_data['dominant_emotion']}</b>"
     return f"For the given statement, the system response is {emotion_data_str}"
 
 @app.route("/")
